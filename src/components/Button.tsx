cf8bcb08605138da138e21343364698798eda535
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-14 19:41:39 +0100

    The submit button works now

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "../App.css";
 
-const Button = () => {
+interface Props {
+  onClick: () => void;
+}
+
+const Button = ({ onClick }: Props) => {
   return (
-    <button className=" text-[12px] tracking-widest rounded-full bg-orangee hover:bg-whitee text-whitee hover:text-orangee hover:animate-pulse py-3 mb-4">
+    <button
+      className=" text-[12px] tracking-widest rounded-full bg-orangee hover:bg-whitee text-whitee hover:text-orangee hover:animate-pulse py-3 mb-4"
+      onClick={onClick}
+    >
       SUBMIT
     </button>
   );
--- src/components/RatingCard.tsx
+++ src/components/RatingCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 import Button from "./Button";
 import RatingNumber from "./RatingNumber";
@@ -9,19 +9,25 @@ import RatingNumber2 from "./RatingNumber2";
 import RatingText2 from "./RatingText2";
 
 const RatingCard = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div className="flex justify-center font-overpass items-center h-screen bg-very-dark-blue">
-      <div className="flex flex-col px-5 py-4 w-[296.5px] gradient text-center rounded-[22px]">
-        <IconHolder />
-        <RatingText />
-        <RatingNumber />
-        <Button />
-      </div>
-      <div className="flex flex-col justify-center items-center px-5 py-4 w-[296.5px] h-[365.25px] gradient text-center rounded-[22px]">
-        <IconHolder2 />
-        <RatingNumber2 />
-        <RatingText2 />
-      </div>
+      {isVisible || (
+        <div className="flex flex-col px-5 py-4 w-[296.5px] gradient text-center rounded-[22px]">
+          <IconHolder />
+          <RatingText />
+          <RatingNumber />
+          <Button onClick={() => setIsVisible(true)} />
+        </div>
+      )}
+      {isVisible && (
+        <div className="flex flex-col justify-center items-center px-5 py-4 w-[296.5px] h-[365.25px] gradient text-center rounded-[22px]">
+          <IconHolder2 />
+          <RatingNumber2 />
+          <RatingText2 />
+        </div>
+      )}
     </div>
   );
 };
