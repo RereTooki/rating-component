6b7dd4970098423e55d22da8074076cd5ad934d3
Rere Tooki <rereloluwa.afolabi.tooki@gmail.com>
2023-08-14 21:15:10 +0100

    Added the focused states

--- src/components/RatingNumber.tsx
+++ src/components/RatingNumber.tsx
@@ -5,19 +5,19 @@ const RatingNumber = () => {
   return (
     <>
       <div className="flex">
-        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce">
+        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce focus:bg-light-grey focus:text-whitee">
           1
         </button>
-        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce">
+        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce focus:bg-light-grey focus:text-whitee">
           2
         </button>
-        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce">
+        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce focus:bg-light-grey focus:text-whitee">
           3
         </button>
-        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce">
+        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce focus:bg-light-grey focus:text-whitee">
           4
         </button>
-        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce">
+        <button className="badge text-light-grey bg-dark-blue hover:bg-orangee hover:text-whitee hover:animate-bounce focus:bg-light-grey focus:text-whitee">
           5
         </button>
       </div>
